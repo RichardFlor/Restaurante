f15f5ac0c7ca38172098bd0fe28bfb050a6b13f2
RichardFlor <richardflor2015@gmail.com>
2025-04-23 12:43:52 -0300

    Redo later.

--- src/main/java/br/senai/sp/defaultproject/security/config/CorsConfig.java
+++ src/main/java/br/senai/sp/defaultproject/security/config/CorsConfig.java
@@ -1,24 +1,26 @@
-package br.senai.sp.defaultproject.security.config;
+//TODO: ARRUMAR DEPOIS
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "http://127.0.0.1:5500",
-                        "http://10.107.131.52:3000" // se tiver um front rodando em outro IP da rede
-                )
-                .allowedHeaders("*")
-                .exposedHeaders("content-range", "total-pages")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS")
-                .allowCredentials(true);
-    }
-}
+//package br.senai.sp.defaultproject.security.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//    @Value("${cht.front.url}")
+//    private String baseUrlFront;
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(
+//                        baseUrlFront,
+//                        "http://localhost:3000"
+//                )
+//                .allowedHeaders("*")
+//                .exposedHeaders("content-range", "total-pages")
+//                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "HEAD", "OPTIONS")
+//                .allowCredentials(true);
+//    }
+//}
