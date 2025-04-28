b7904322a9fb56ca55398d31e6dc52d1f50f16fb
RichardFlor <richardflor2015@gmail.com>
2025-04-27 23:09:28 -0300

    Add Cors configuration.

--- src/main/java/br/senai/sp/defaultproject/security/config/CorsConfig.java
+++ src/main/java/br/senai/sp/defaultproject/security/config/CorsConfig.java
@@ -1,26 +1,53 @@
 //TODO: ARRUMAR DEPOIS
 
-//package br.senai.sp.defaultproject.security.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
+package br.senai.sp.defaultproject.security.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 //@Configuration
 //public class CorsConfig implements WebMvcConfigurer {
 //    @Value("${cht.front.url}")
 //    private String baseUrlFront;
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins(
-//                        baseUrlFront,
-//                        "http://localhost:3000"
-//                )
-//                .allowedHeaders("*")
-//                .exposedHeaders("content-range", "total-pages")
-//                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "HEAD", "OPTIONS")
-//                .allowCredentials(true);
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration cors = new CorsConfiguration();
+//        cors.setAllowedOrigins(Collections.singletonList(baseUrlFront));
+//        cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+//        cors.setExposedHeaders(List.of("*"));
+//        cors.setAllowedHeaders(List.of("*"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", cors);
+//        return source;
 //    }
 //}
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Value("${cht.front.url}")
+    private String baseUrlFront;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedOrigins(Collections.singletonList(baseUrlFront));
+        cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        cors.setExposedHeaders(List.of("*"));
+        cors.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+        return source;
+    }
+}
